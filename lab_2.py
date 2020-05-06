282f0c9f534a2fb0ddb9f52b16a639f037387e85
Myaskorzz <62871874+Myaskorzz@users.noreply.github.com>
2020-05-06 18:42:41 +1000

    Add files via upload

---
+++ lab_2.py
@@ -0,0 +1,36 @@
+data={}
+flag = True 
+initfile  = r'C:\Users\79940\Desktop\Projects\conf.txt'
+with open(initfile , 'r') as f:
+    for line in f:
+        if line[0] != "#" and line[0] !=';' and line[0] != '\n':
+            list = line.split(' ', 1)
+            x=len(list)
+            if x > 1:
+                key = list[0]
+                value = list[1]
+                data[key]=value 
+while flag:
+    try: 
+        key = input("Введите ключ>>> ")
+        print ("Ключ>>>", key," Значение>>>", data[key])
+    except:
+        print("Ключ>>>", key," Значение>>> Такого ключа не существует или значение не задано")
+    counter = 0
+    i = 2
+    while True:
+        if counter == 3:
+            exit()
+        command = input("Повторить? Да\Нет>>> ")
+        if command == "Да":
+            break
+        elif command == "Нет":
+            flag = False 
+            break
+        else: 
+            if i == 0:
+                print("Попытки кончились,программа завершается ")
+            print("Некоректный ввод. Попыток осталось>>>",i)   
+            counter+=1 
+            i-=1
+            
